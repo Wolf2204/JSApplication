080d38759960b339291593ccdd9b0ef6ead24f41
Wolf <volkov911@tut.by>
2017-07-15 14:27:40 +0300

    test

--- js/Game.js
+++ js/Game.js
@@ -10,21 +10,19 @@
     deck.deckList(arr1, arr2, arr3, arr4);
 
     let deckList = arr1.concat(arr2, arr3, arr4),
-    gameDeck = deck.randomDeck(deckList);
+        gameDeck = deck.randomDeck(deckList);
     deck.suitValue(gameDeck);
     console.log(gameDeck);
 
     let computerHand = [],
         playerHand = [];
-    let deck1 = document.querySelector('.deck1'),
+    let deckForGame = document.querySelector('#deck'),
+        deck1 = document.querySelector('.deck1'),
         deck2 = document.querySelector('.deck2');
     let players = new Players();
-    players.handPlayers(computerHand, playerHand, gameDeck, deck1, deck2);
+    players.handPlayers(computerHand, playerHand, gameDeck, deck1, deck2, deckForGame);
     console.log(computerHand);
     console.log(playerHand);
-
-
-
 
 
     function Game() {
@@ -32,11 +30,65 @@
     }
 
     Object.defineProperties(Game.prototype, {
-        game: function () {
+        cardPosition: {
+            value: function (computerHand, playerHand, gameDeck) {
 
+                let cardsForGame = document.querySelectorAll('#deck img');
+                let src = 'images/cardback.jpg';
+                cardsForGame = [...cardsForGame];
+                let position = 1120;
+                cardsForGame.forEach(function (v) {
+                    v.src = src;
+                    v.style.left = `${position}px`;
+                    position += 1
+                });
+                console.log(cardsForGame);
+                cardsForGame[0].src = gameDeck[0].card;
+                cardsForGame[0].classList.add('suit');
+                cardsForGame[0].style.left = '1080px';
+
+
+                let computerCards = document.querySelectorAll('.deck1 img');
+                let playerCards = document.querySelectorAll('.deck2 img');
+                let position1 = 100;
+                computerCards.forEach(function (v) {
+                    v.src = src;
+                    v.style.left = `${position1}px`;
+                    position1 += 40
+                });
+                let position2 = 100;
+                playerCards.forEach(function (v, i) {
+                    v.src = playerHand[i].card;
+                    v.style.left = `${position2}px`;
+                    position2 += 40
+                });
+                console.log(computerCards);
+                console.log(playerCards);
+            }
+        },
+
+        startGame: {
+          value: function () {
+
+          }
+        },
+
+        turnPlayer: {
+          value: function () {
+
+          }
+        },
+
+        turnComputer: {
+            value: function () {
+
+            }
         }
+
     });
 
 
     let gameDurak = new Game();
+
+    gameDurak.cardPosition(computerHand, playerHand, gameDeck);
 }());
--- js/Players.js
+++ js/Players.js
@@ -7,21 +7,27 @@
 
     Object.defineProperties(Players.prototype, {
         handPlayers: {
-            value: function (computer, player, deck, deck1, deck2) {
+            value: function (computer, player, deck, deck1, deck2, deckOnPage) {
                 //let position = 50;
                 for (let i = 0; i < 6; i++) {
-                    let card1 = document.createElement('img');
-                    let card2 = document.createElement('img');
+                    let card1 = document.createElement('img'),
+                        card2 = document.createElement('img');
                     let a = deck.pop();
                     let b = deck.pop();
                     computer.push(a);
                     player.push(b);
                     deck1.appendChild(card1);
                     deck2.appendChild(card2);
+
+
                     //deck1[i].style.left(`${position}px`);
                     //deck2[i].style.left(`${position}px`);
                     //position +=20
                 }
+                deck.forEach(function () {
+                    let a = document.createElement('img');
+                    deckOnPage.appendChild(a)
+                });
 
 
             }
